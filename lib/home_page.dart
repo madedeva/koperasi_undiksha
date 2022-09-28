ebf5461125ec71ac65eee91c02cbaf54ae1b2435
madedeva <deva.kerti@undiksha.ac.id>
2022-09-28 22:06:05 +0800

    membuat halaman beranda

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,7 +17,131 @@ class _HomeState extends State<Home> {
         backgroundColor: const Color.fromARGB(255, 33, 54, 243), //appbar color
         title: const Text('Koperasi Undiksha'),
       ),
-      body: Center(),
+      body: SingleChildScrollView(
+        child: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  border: Border.all(
+                    color: const Color.fromARGB(255, 10, 7, 139),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      child: const Image(
+                        image: AssetImage('images/hansik.jpg'),
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 200,
+                            padding: EdgeInsets.all(20.0),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                color: Color.fromARGB(255, 206, 191, 238)),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Nasabah',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold)),
+                                Text('Deva Kerti Wijaya'),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10.0,
+                          ),
+                          Container(
+                            width: 200,
+                            decoration: BoxDecoration(),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Container(
+                                  width: 200,
+                                  padding: EdgeInsets.all(20.0),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      color:
+                                          Color.fromARGB(255, 206, 191, 238)),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text('Total Saldo Anda',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold)),
+                                      Text('Rp. 1.000.000'),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  border: Border.all(
+                    color: const Color.fromARGB(255, 10, 7, 139),
+                  ),
+                ),
+                child: Row(
+                  children: [Text('data')],
+                ),
+              )
+            ],
+          ),
+        )),
+      ),
+      //bottom navigation bar
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color.fromARGB(255, 33, 54, 243),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Setting',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        showUnselectedLabels: true,
+      ),
     );
   }
 }
